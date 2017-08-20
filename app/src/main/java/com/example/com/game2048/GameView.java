72d350c73e991062d897de04ee344ebcbcaa6648
IceH <719205998@qq.com>
2017-08-20 14:00:17 +0800

    实现游戏逻辑,完成结束判定

--- app/src/main/java/com/example/com/game2048/GameView.java
+++ app/src/main/java/com/example/com/game2048/GameView.java
@@ -1,11 +1,21 @@
 package com.example.com.game2048;
 
+import android.graphics.Point;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.GridLayout;
 import android.content.Context;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 
 /**
@@ -17,6 +27,12 @@ public class GameView extends GridLayout {
     private final int m_left=0,m_right=1,m_up=2,m_down=3;
 
     private Card [][] cards = new Card[4][4];
+
+    private List<Point> emptyPoints = new ArrayList<Point>();
+
+
+
+
 
     public GameView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -86,7 +102,6 @@ public class GameView extends GridLayout {
                 }
 
 
-
                 return true;
             }
         });
@@ -100,6 +115,7 @@ public class GameView extends GridLayout {
         super.onSizeChanged(w,h,oldh,oldh);
         int cardwidth=(Math.min(w,h)-10)/4;
         addCards(cardwidth,cardwidth);
+        gamestart();
 
     }
 
@@ -112,7 +128,7 @@ public class GameView extends GridLayout {
             for(int x=0;x<4;x++)
             {
                 c=new Card(getContext());
-                c.setNum(2);
+                c.setNum(0);
                 addView(c,width,height);
 
                 cards[x][y]=c;
@@ -122,7 +138,457 @@ public class GameView extends GridLayout {
 
     private void move(int direc)
     {
+        boolean flag =false;
+
+        switch (direc)
+        {
+            case m_left:
+                flag=MoveLeft();
+                break;
+            case m_right:
+                flag=MoveRight();
+                break;
+            case m_up:
+                flag=MoveUp();
+                break;
+            case m_down:
+                flag=MoveDown();
+                break;
+        }
+        if(flag==true)
+        {
+            if(addRandomNum()==false)//添加卡片并验证游戏是否结束,,,,,,逻辑错误,无法验证
+            {
+                checkgameover();
+            }
+
+        }
+        else
+        {
+            //移动失败,没有可以移动的卡片
+        }
+
 
     }
 
+    private void checkgameover()//在卡片填满后检验还能否消去
+    {
+        for(int y=0;y<4;y++)
+        {
+            for(int x=1;x<4;x++)
+            {
+                if(cards[x-1][y].getNum()==cards[x][y].getNum())
+                {
+                    return;//可消去
+                }
+            }
+        }
+
+        for(int x=0;x<4;x++)
+        {
+            for(int y=1;y<4;y++)
+            {
+                if(cards[x][y-1].getNum()==cards[x][y].getNum())
+                {
+                    return;//可消去
+                }
+            }
+        }
+
+        //不可消去
+        MainActivity.getMainActivity().gameover();
+    }
+
+
+    private void gamestart()
+    {
+        for (int y=0;y<4;y++)
+        {
+            for (int x=0;x<4;x++)
+            {
+                cards[x][y].setNum(0);
+            }
+        }
+        MainActivity.num_score=0;
+        addRandomNum();
+        addRandomNum();
+
+    }
+
+
+
+    private boolean addRandomNum()
+    {
+        emptyPoints.clear();
+        for (int y=0;y<4;y++)
+        {
+            for (int x=0;x<4;x++)
+            {
+                if(cards[x][y].getNum()==0)
+                {
+                    emptyPoints.add(new Point(x,y));
+                }
+            }
+        }
+
+
+
+        Point point=emptyPoints.remove((int)(Math.random()*emptyPoints.size()));
+
+        cards[point.x][point.y].setNum(Math.random()>0.1?2:4);
+
+        if(emptyPoints.size()==0)
+        {
+
+            return false;
+        }
+
+        return true;
+
+    }
+
+
+    private boolean MoveLeft()
+    {
+        boolean flag=false;
+
+        for(int y=0;y<4;y++)
+        {
+
+
+            for(int x=1;x<4;x++)//尝试移动
+            {
+                if(cards[x][y].getNum()!=0)//有一个卡片尝试移动
+                {
+                    int xx;
+                    for(xx=x-1;xx>=0;xx--)
+                    {
+                        if (cards[xx][y].getNum()!=0)//遇到阻碍,移动结束
+                        {
+                            break;
+                        }
+                    }
+
+                    xx++;
+                    if(xx==x)//移动失败
+                    {
+
+                    }
+                    else//移动成功
+                    {
+                        cards[xx][y].setNum(cards[x][y].getNum());
+                        cards[x][y].setNum(0);
+                        flag=true;
+                    }
+
+
+                }
+            }
+
+            for(int x=1;x<4;x++)//尝试消除
+            {
+                if(cards[x-1][y].getNum()==cards[x][y].getNum()&&cards[x][y].getNum()!=0)//消除成功
+                {
+                    MainActivity.getMainActivity().addScore(cards[x][y].getNum()*2);
+                    cards[x-1][y].setNum(cards[x][y].getNum()*2);
+                    cards[x][y].setNum(0);
+                    flag=true;
+                }
+            }
+
+            for(int x=1;x<4;x++)//可能发生了消除,再次尝试移动
+            {
+                if(cards[x][y].getNum()!=0)//有一个卡片尝试移动
+                {
+                    int xx;
+                    for(xx=x-1;xx>=0;xx--)
+                    {
+                        if (cards[xx][y].getNum()!=0)//遇到阻碍,移动结束
+                        {
+                            break;
+                        }
+                    }
+
+                    xx++;
+                    if(xx==x)//移动失败
+                    {
+
+                    }
+                    else//移动成功
+                    {
+                        cards[xx][y].setNum(cards[x][y].getNum());
+                        cards[x][y].setNum(0);
+                        flag=true;
+                    }
+
+
+                }
+            }
+
+        }
+
+
+        if(flag==false)
+            return false;
+        else
+            return true;
+    }
+
+    private boolean MoveRight()
+    {
+        boolean flag=false;
+
+        for(int y=0;y<4;y++)
+        {
+
+
+            for(int x=2;x>=0;x--)//尝试移动
+            {
+                if(cards[x][y].getNum()!=0)//有一个卡片尝试移动
+                {
+                    int xx;
+                    for(xx=x+1;xx<=3;xx++)
+                    {
+                        if (cards[xx][y].getNum()!=0)//遇到阻碍,移动结束
+                        {
+                            break;
+                        }
+                    }
+
+                    xx--;
+                    if(xx==x)//移动失败
+                    {
+
+                    }
+                    else//移动成功
+                    {
+                        cards[xx][y].setNum(cards[x][y].getNum());
+                        cards[x][y].setNum(0);
+                        flag=true;
+                    }
+
+
+                }
+            }
+
+            for(int x=2;x>=0;x--)//尝试消除
+            {
+                if(cards[x+1][y].getNum()==cards[x][y].getNum()&&cards[x][y].getNum()!=0)//消除成功
+                {
+                    MainActivity.getMainActivity().addScore(cards[x][y].getNum()*2);
+                    cards[x+1][y].setNum(cards[x][y].getNum()*2);
+                    cards[x][y].setNum(0);
+                    flag=true;
+                }
+            }
+
+            for(int x=2;x>=0;x--)//可能发生了消除,再次尝试移动
+            {
+                if(cards[x][y].getNum()!=0)//有一个卡片尝试移动
+                {
+                    int xx;
+                    for(xx=x+1;xx<=3;xx++)
+                    {
+                        if (cards[xx][y].getNum()!=0)//遇到阻碍,移动结束
+                        {
+                            break;
+                        }
+                    }
+
+                    xx--;
+                    if(xx==x)//移动失败
+                    {
+
+                    }
+                    else//移动成功
+                    {
+                        cards[xx][y].setNum(cards[x][y].getNum());
+                        cards[x][y].setNum(0);
+                        flag=true;
+                    }
+
+
+                }
+            }
+
+        }
+
+
+        if(flag==false)
+            return false;
+        else
+            return true;
+    }
+
+    private boolean MoveUp()
+    {
+        boolean flag=false;
+
+        for(int x=0;x<4;x++)
+        {
+
+
+            for(int y=1;y<4;y++)//尝试移动
+            {
+                if(cards[x][y].getNum()!=0)//有一个卡片尝试移动
+                {
+                    int yy;
+                    for(yy=y-1;yy>=0;yy--)
+                    {
+                        if (cards[x][yy].getNum()!=0)//遇到阻碍,移动结束
+                        {
+                            break;
+                        }
+                    }
+
+                    yy++;
+                    if(yy==y)//移动失败
+                    {
+
+                    }
+                    else//移动成功
+                    {
+                        cards[x][yy].setNum(cards[x][y].getNum());
+                        cards[x][y].setNum(0);
+                        flag=true;
+                    }
+
+
+                }
+            }
+
+            for(int y=1;y<4;y++)//尝试消除
+            {
+                if(cards[x][y-1].getNum()==cards[x][y].getNum()&&cards[x][y].getNum()!=0)//消除成功
+                {
+                    MainActivity.getMainActivity().addScore(cards[x][y].getNum()*2);
+                    cards[x][y-1].setNum(cards[x][y].getNum()*2);
+                    cards[x][y].setNum(0);
+                    flag=true;
+                }
+            }
+
+            for(int y=1;y<4;y++)//可能发生了消除,再次尝试移动
+            {
+                if(cards[x][y].getNum()!=0)//有一个卡片尝试移动
+                {
+                    int yy;
+                    for(yy=y-1;yy>=0;yy--)
+                    {
+                        if (cards[x][yy].getNum()!=0)//遇到阻碍,移动结束
+                        {
+                            break;
+                        }
+                    }
+
+                    yy++;
+                    if(yy==y)//移动失败
+                    {
+
+                    }
+                    else//移动成功
+                    {
+                        cards[x][yy].setNum(cards[x][y].getNum());
+                        cards[x][y].setNum(0);
+                        flag=true;
+                    }
+
+
+                }
+            }
+
+        }
+
+
+        if(flag==false)
+            return false;
+        else
+            return true;
+    }
+
+    private boolean MoveDown()
+    {
+        boolean flag=false;
+
+        for(int x=0;x<4;x++)
+        {
+
+
+            for(int y=2;y>=0;y--)//尝试移动
+            {
+                if(cards[x][y].getNum()!=0)//有一个卡片尝试移动
+                {
+                    int yy;
+                    for(yy=y+1;yy<=3;yy++)
+                    {
+                        if (cards[x][yy].getNum()!=0)//遇到阻碍,移动结束
+                        {
+                            break;
+                        }
+                    }
+
+                    yy--;
+                    if(yy==y)//移动失败
+                    {
+
+                    }
+                    else//移动成功
+                    {
+                        cards[x][yy].setNum(cards[x][y].getNum());
+                        cards[x][y].setNum(0);
+                        flag=true;
+                    }
+
+
+                }
+            }
+
+            for(int y=2;y>=0;y--)//尝试消除
+            {
+                if(cards[x][y+1].getNum()==cards[x][y].getNum()&&cards[x][y].getNum()!=0)//消除成功
+                {
+                    MainActivity.getMainActivity().addScore(cards[x][y].getNum()*2);
+                    cards[x][y+1].setNum(cards[x][y].getNum()*2);
+                    cards[x][y].setNum(0);
+                    flag=true;
+                }
+            }
+
+            for(int y=2;y>=0;y--)//可能发生了消除,再次尝试移动
+            {
+                if(cards[x][y].getNum()!=0)//有一个卡片尝试移动
+                {
+                    int yy;
+                    for(yy=y+1;yy<=3;yy++)
+                    {
+                        if (cards[x][yy].getNum()!=0)//遇到阻碍,移动结束
+                        {
+                            break;
+                        }
+                    }
+
+                    yy--;
+                    if(yy==y)//移动失败
+                    {
+
+                    }
+                    else//移动成功
+                    {
+                        cards[x][yy].setNum(cards[x][y].getNum());
+                        cards[x][y].setNum(0);
+                        flag=true;
+                    }
+
+
+                }
+            }
+
+        }
+
+
+        if(flag==false)
+            return false;
+        else
+            return true;
+    }
 }
--- app/src/main/java/com/example/com/game2048/MainActivity.java
+++ app/src/main/java/com/example/com/game2048/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.com.game2048;
 
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private static TextView t_score;
+
+    public static int num_score=0;
+
+    private static MainActivity mainActivity = null;
+
+    public MainActivity()
+    {
+        mainActivity = this;
+    }
+
+    public static MainActivity getMainActivity()
+    {
+        return mainActivity;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t_score=(TextView)findViewById(R.id.score);
+    }
+
+    public void addScore(int add)
+    {
+        num_score+=add;
+        t_score.setText(""+num_score);
+
+    }
+
+
+    public void gameover()
+    {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setTitle("GG");
+        dialog.setMessage("Game Over");
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("OK",new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface,int which)
+                    {
+
+                    }
+                }
+        ).show();
     }
 }
+
